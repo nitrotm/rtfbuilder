f7d8acf754818f3d818a649976b8049764119cf5
Antony Ducommun <antony@aducommun.ch>
2025-09-14 10:58:40 +0200

    rebuild embedded jsx-dev-runtime

--- jsx-dev-runtime/jsx-dev-runtime.d.ts
+++ jsx-dev-runtime/jsx-dev-runtime.d.ts
@@ -1,4 +1,19 @@
 export declare const Comment: () => undefined;
 export declare const CData: () => undefined;
-export declare const jsx: (name: string | ((props: unknown) => unknown), { children, ...props }: Record<string, unknown>) => unknown;
+declare class XmlComment {
+    content: unknown;
+    constructor(content: unknown);
+    toString(): string;
+}
+declare class XmlCData {
+    content: unknown;
+    constructor(content: unknown);
+    toString(): string;
+}
+declare class XmlFragment {
+    children: (Element | XmlComment | XmlCData | string)[];
+    constructor(children: (Element | XmlComment | XmlCData | string)[]);
+    toString(): string;
+}
+export declare const jsx: (name: string | ((props: unknown) => unknown), { children, ...props }: Record<string, unknown>) => XmlFragment | XmlComment | XmlCData;
 export { jsx as jsxs, jsx as jsxDEV };
